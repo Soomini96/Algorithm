968fc2eb232d31c127047a37bc44f298c699b649
수민 <91924105+Soomini96@users.noreply.github.com>
2022-12-16 09:43:21 +0900

    [Bronze III] Title: 최댓값, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main { // 2562
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int max = 0;
+		int idx = 0;
+		for (int i = 0; i < 9; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (max < num) {
+				max = num;
+				idx = i + 1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(idx);
+	}
+}
